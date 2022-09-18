a2c8e5be5c4d8d67f735b03bd4c750a748344e47
AHenry2117 <Aehenry2117@gmail.com>
2022-09-18 14:29:33 -0700

    added images

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,10 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
+              <img src="/images/skeleton404image.jpg" alt="Skeleton Blue Colar Suit"/>
+              <div>
+              Photo by <a href="https://unsplash.com/@arturtumasjan?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Artur Tumasjan</a> on <a href="https://unsplash.com/s/photos/funny-images?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </div>
               <p>Oops, sorry, we can't find this page!</p>
           </main>
       </Def>
