e0b186c94fc5edde2d4af8f994eb98ba97d54d3f
AHenry2117 <Aehenry2117@gmail.com>
2022-09-29 18:04:13 -0700

    pull git

--- controllers/places.js
+++ controllers/places.js
@@ -1,33 +1,32 @@
-const router = require('express').Router();
-
-
 // GET /places
+const router = require('express').Router()
+const places = require('../models/places.js')
+
 router.get('/', (req, res) => {
-  let places= [
-   {
-    name: "H-Thai-ML",
-    city: "Orlando",
-    state: "FL",
-    cuisines: "Thai, Pan-Asian",
-    pic: "/images/pad-thai.jpg",
-   },
-   {
-    name: "Coding Cat Cafe",
-    city: "Hoboken",
-    state: "NJ",
-    cuisines: "Coffee, Bakery",
-    pic: "/images/Coffee.jpg",
-   }];
-  res.render('places/index', {places});
-});
+    res.render('places/index', { places })
+})
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 });
 router.post('/', (req, res) => {
   console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect("/places")
   res.send('POST /places')
 })
+
 
 
 
