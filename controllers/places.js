31c29c560bb49cc15f01c7b12a0de41a3dbe5363
AHenry2117 <Aehenry2117@gmail.com>
2022-10-03 21:24:52 -0700

    added seeders

--- controllers/places.js
+++ controllers/places.js
@@ -29,8 +29,10 @@ router.get("/new", (req, res) => {
 
 router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
+    .populate('comments')
     .then((place) => {
       res.render("places/show", { place });
+      console.log(place.comments)
     })
     .catch((err) => {
       console.log("err", err);
@@ -57,5 +59,32 @@ router.post("/:id/rant", (req, res) => {
 router.delete("/:id/rant/:rantId", (req, res) => {
   res.send("GET /places/:id/rant/:rantId stub");
 });
+
+router.post("/:id/comment", (req, res) => {
+  if (req.body.rant) {
+    req.body.rant = true;
+  } else {
+    req.body.rant = false;
+  }
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      console.log(db.Comment);
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place.save().then(() => {
+            res.redirect(`/places/${req.params.id}`);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
+});
+
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
@@ -6,3 +7,4 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 
 module.exports.Place = require('./places')
+module.exports.Comment = require('./comment')
